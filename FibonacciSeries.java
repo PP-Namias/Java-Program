b0ff8ce492dbd92232b89945c8a7aa7116e91e8a
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-01-25 00:19:10 +0800

    :sparkles: Update FibonacciSeries.java :sparkles:

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,20 +1,20 @@
 public class FibonacciSeries {
-  
-  static int n1 = 0, n2 = 1, n3 = 0;
 
-  static void printFibonacci (int count) {
-      if ( count > 0 ) {
-          n3 = n1 + n2;
-          n1 = n2;
-          n2 = n3;
-          System.out.print (" " + n3);
-          printFibonacci (count - 1);
-      }
-  }
+    static long n1 = 0, n2 = 1, n3 = 0;
 
-  public static void main (String[] args) {
-      int count = 25;
-      System.out.print (n1 + " " + n2); //printing 0 and 1
-      printFibonacci (count - 2); //n-2 because 2 numbers are already printed
-  }
+    static void printFibonacci (int count) {
+        if ( count > 0 ) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.print (" " + n3);
+            printFibonacci (count - 1);
+        }
+    }
+
+    public static void main (String[] args) {
+        int count = 50;
+        System.out.print (n1 + " " + n2); //printing 0 and 1
+        printFibonacci (count - 2); //n-2 because 2 numbers are already printed
+    }
 }
