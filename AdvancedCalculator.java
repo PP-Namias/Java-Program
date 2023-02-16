073caf13e112cbd2aa336962a0555441b9b0be47
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-02-16 19:19:11 +0800

    :sparkles: Update AdvancedCalculator.java :sparkles:

--- AdvancedCalculator.java
+++ AdvancedCalculator.java
@@ -16,12 +16,35 @@
  */
 /*
  * Features:
- *  -> Add multipole numbers
+ *  -> Add multiple numbers
  *  -> loops infinitly
  *  -> Gets the operator (+, -, *, /)
  *  -> View the result
  *  -> Exit
  *  -> Error handling
+ * 
+ * 
+ * Future Features:
+ * Make an whole statement for the result and then print it out at the end of the loop
+ *  -> Add multiple numbers
+ *  -> Make an equation for the result
+ *  -> View the result
+ *  -> Exit
+ * 
+ * Example: 
+ *  1 + 2 + 3 + 4 + 5 = 15
+ *  2 * 5 / 10 + 3 = 3.5
+ *  6 - 5 + 4 - 3 + 2 - 1 = 1
+ *  1 + 2 * 3 = 7
+ * 
+ * Make an char that allows the user to input the equation 
+ * and then split it up into an array of strings 
+ * and then parse it into an array of doubles 
+ * and then do the calculations and then print out the result 
+ * and then loop it back to the start of the program 
+ * and then make an exit statement 
+ * and then make an error handling statement 
+ * and then make an equation statement
 */  
 import java.util.Scanner;
 
@@ -50,22 +73,50 @@ public class AdvancedCalculator {
 
         for (int i = 1; i < numbers.length; i++) {
           if (operation.equals("+")) {
+            if (i == 1) {
+              System.out.print(numbers[0]);
+              System.out.print(" + " + numbers[1]);
+            }
+            else if (i >= 1) {
+              System.out.print(" + " + numbers[i]);
+            }
             result += numbers[i];
           } else if (operation.equals("-")) {
+            if (i == 1) {
+              System.out.print(numbers[0]);
+              System.out.print(" - " + numbers[1]);
+            }
+            else if (i >= 1) {
+              System.out.print(" - " + numbers[i]);
+            }
             result -= numbers[i];
           } else if (operation.equals("*")) {
+            if (i == 1) {
+              System.out.print(numbers[0]);
+              System.out.print(" * " + numbers[1]);
+            }
+            else if (i >= 1) {
+              System.out.print(" * " + numbers[i]);
+            }
             result *= numbers[i];
           } else if (operation.equals("/")) {
             if (numbers[i] == 0) {
               System.out.println("Cannot divide by zero");
               break;
             } else {
+              if (i == 1) {
+                System.out.print(numbers[0]);
+                System.out.print(" / " + numbers[1]);
+              }
+              else if (i >= 1) {
+                System.out.print(" / " + numbers[i]);
+              }
               result /= numbers[i];
             }
           }
         }
 
-        System.out.println("Result: " + result);
+        System.out.println(" = " + result);
       } else {
         System.out.println("Invalid operation");
       }
