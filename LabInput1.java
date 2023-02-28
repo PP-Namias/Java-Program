1772c8bb8dbcc09026ec4330a76396ab4916d7bd
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-02-28 10:52:24 +0800

    Program Correction

--- LabInput1.java
+++ LabInput1.java
@@ -1,17 +1,25 @@
 /**
- * Seconds Converter Activity #1
+ * LabInput1.java
+ * Time Converter Activity #1
  * @author PP-Namias
  * Licensed under MIT License
  */
-
+/** 
+ * @param [input] the scanner for user input.
+ * @param [seconds] The number of seconds input.
+ * @param [hours] the number of hours.
+ * @param [minutes] the number of minutes.
+ * @param [secondsRemainder] the number of seconds remainder.
+ * @param [exit] the boolean to check if the program will exit.
+ */
 /*
  * Write a program that accepts input to varible seconds.
  * The program will then output the equivalent time in hours:minutes:seconds format.
  * use labInput1 as classname and filename.
  * 
  * Test ouput:
- * Please enter number of seconds: 4870
- * 4870 seconds = 1:21:10
+ *    Please enter number of seconds: 4870
+ *    4870 seconds = 1:21:10
  */
 import java.util.Scanner;
 
@@ -19,11 +27,26 @@ public class LabInput1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int seconds, hours, minutes, secondsRemainder;
-    System.out.print("Please enter number of seconds: ");
-    seconds = input.nextInt();
-    hours = seconds / 3600;
-    minutes = (seconds % 3600) / 60;
-    secondsRemainder = (seconds % 3600) % 60;
-    System.out.println(seconds + " seconds = " + hours + ":" + minutes + ":" + secondsRemainder);
+    boolean exit = false;
+
+    while (!exit) {
+      try {
+        System.out.print("Please enter number of seconds or enter '0' to quit: ");
+        seconds = input.nextInt();
+        if (seconds == 0) {
+          System.out.print("Exiting the program! Thank you for using this program!");
+          exit = true;
+        }
+        else {
+          hours = seconds / 3600;
+          minutes = (seconds % 3600) / 60;
+          secondsRemainder = (seconds % 3600) % 60;
+          System.out.println(seconds + " seconds = " + hours + ":" + minutes + ":" + secondsRemainder);
+        }
+      } catch (Exception e) {
+        System.out.println("Please enter the correct input.");
+      }
+    }
+    input.close();
   }
 }
--- LabInput2.java
+++ LabInput2.java
@@ -1,12 +1,26 @@
+/**
+ * LabInput2.java
+ * Time Interpreter Activity #2
+ * @author PP-Namias
+ * Licensed under MIT License
+ */
+/** 
+ * @param [input] the scanner for user input.
+ * @param [num] the number that indicates the number of that needs to be interperted.
+ * @param [hundreds] the number of hundreds.
+ * @param [tens] the number of tens.
+ * @param [ones] the number of ones.
+ * @param [exit] the boolean to check if the program will exit.
+ */
 /*
  * Write a program that inputs a three-digit value into variable num
- * and then display each digit with the correspobding place value. A
+ * and then display each digit with the corresponding place value. A
  * sample output is given below. Use LabInput2 as classname and filename 
  * 
  * Test output:
- * Please enter a three-digit integer: 487
- * The value of num = 487
- * 4 hundreds 8 tens and 7 ones
+ *    Please enter a three-digit integer: 487
+ *    The value of num = 487
+ *    4 hundreds 8 tens and 7 ones
  */
 import java.util.Scanner;
 
@@ -14,12 +28,27 @@ public class LabInput2 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int num, hundreds, tens, ones;
-    System.out.print("Please enter a three-digit integer: ");
-    num = input.nextInt();
-    hundreds = num / 100;
-    tens = (num % 100) / 10;
-    ones = (num % 100) % 10;
-    System.out.println("The value of num = " + num);
-    System.out.println(hundreds + " hundreds " + tens + " tens and " + ones + " ones");
+    boolean exit = false;
+
+    while (!exit) {
+      try {
+        System.out.print("Please enter a three-digit integer or enter '0' to quit: ");
+        num = input.nextInt();
+        if (num == 0) {
+          System.out.print("Exiting the program! Thank you for using this program!");
+          exit = true;
+        }
+        else {
+          hundreds = num / 100;
+          tens = (num % 100) / 10;
+          ones = (num % 100) % 10;
+          System.out.println("The value of num = " + num);
+          System.out.println(hundreds + " hundreds " + tens + " tens and " + ones + " ones");
+        }
+      } catch (Exception e) {
+        System.out.println("Please enter the correct input.");
+      }
+    }
+  input.close();
   }
 }
